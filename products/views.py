64b26a2c0a20e5a761d414ba55732cf564b9f7d6
martinbannister <martin.bannister91@gmail.com>
2022-02-07 15:16:57 +0000

    start product template

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ def all_products(request):
     products = Product.objects.all()
 
     context = {
-        'product': products,
+        'products': products,
     }
 
     return render(request, 'products/products.html', context)
