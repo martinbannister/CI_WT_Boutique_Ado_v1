3581700d336340ae4b51524ad83d3e808e43f538
martinbannister <martin.bannister91@gmail.com>
2022-02-10 15:39:44 +0000

    update context processor

--- bag/views.py
+++ bag/views.py
@@ -19,6 +19,5 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-        request.session['bag'] = bag
-        print(request.session['bag'])
-        return redirect(redirect_url)
+    request.session['bag'] = bag
+    return redirect(redirect_url)
